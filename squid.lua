c1b04ea948503cfa28a91b1be2a533e2356a237e
2garlicbread <155115950+2garlicbread@users.noreply.github.com>
2024-04-13 18:30:20 +1200

    Create squid.lua

---
+++ squid.lua
@@ -0,0 +1,261 @@
+--[[
+
+    squid.lua
+
+    Checks if a string is a mispelling/variation of the word 'squid'. 
+
+    @   2garlicbread
+    #   13.04.2024
+    /   https://github.com/2garlicbread/useless-shit/squid.lua
+
+]]--   
+return function isSquid(word: string): boolean
+    if word == "squid" then
+        return true
+    end
+    if word == "squd" then
+        return true
+    end
+    if word == "sqid" then
+        return true
+    end
+    if word == "sqwid" then
+        return true
+    end
+    if word == "squuid" then
+        return true
+    end
+    if word == "squud" then
+        return true
+    end
+    if word == "sckwid" then
+        return true
+    end
+    if word == "skwid" then
+        return true
+    end
+    if word == "skwid" then
+        return true
+    end
+    if word == "squed" then
+        return true
+    end
+    if word == "sqwed" then
+        return true
+    end
+    if word == "sqwud" then
+        return true
+    end
+    if word == "squidd" then
+        return true
+    end
+    if word == "sqiud" then
+        return true
+    end
+    if word == "squyud" then
+        return true
+    end
+    if word == "squiyd" then
+        return true
+    end
+    if word == "sguid" then
+        return true
+    end
+    if word == "squiyd" then
+        return true
+    end
+    if word == "squiddd" then
+        return true
+    end
+    if word == "squidddd" then
+        return true
+    end
+    if word == "sqiud" then
+        return true
+    end
+    if word == "squiyd" then
+        return true
+    end
+    if word == "sqiuid" then
+        return true
+    end
+    if word == "sguid" then
+        return true
+    end
+    if word == "squed" then
+        return true
+    end
+    if word == "sqwud" then
+        return true
+    end
+    if word == "sqiuid" then
+        return true
+    end
+    if word == "sguid" then
+        return true
+    end
+    if word == "squed" then
+        return true
+    end
+    if word == "squud" then
+        return true
+    end
+    if word == "squiyd" then
+        return true
+    end
+    if word == "squuid" then
+        return true
+    end
+    if word == "sqiuid" then
+        return true
+    end
+    if word == "squid" then
+        return true
+    end
+    if word == "squd" then
+        return true
+    end
+    if word == "sqwid" then
+        return true
+    end
+    if word == "squuid" then
+        return true
+    end
+    if word == "squed" then
+        return true
+    end
+    if word == "sqwud" then
+        return true
+    end
+    if word == "squiid" then
+        return true
+    end
+    if word == "sqqid" then
+        return true
+    end
+    if word == "sqidq" then
+        return true
+    end
+    if word == "sqiud" then
+        return true
+    end
+    if word == "squir" then
+        return true
+    end
+    if word == "squik" then
+        return true
+    end
+    if word == "sqeud" then
+        return true
+    end
+    if word == "sqoood" then
+        return true
+    end
+    if word == "sqiigd" then
+        return true
+    end
+    if word == "sqabnd" then
+        return true
+    end
+    if word == "sqauiud" then
+        return true
+    end
+    if word == "sqewod" then
+        return true
+    end
+    if word == "sqeuwid" then
+        return true
+    end
+    if word == "sqooid" then
+        return true
+    end
+    if word == "sqaquid" then
+        return true
+    end
+    if word == "squibn" then
+        return true
+    end
+    if word == "sqhd" then
+        return true
+    end
+    if word == "sqzkd" then
+        return true
+    end
+    if word == "sqopd" then
+        return true
+    end
+    if word == "sqierd" then
+        return true
+    end
+    if word == "sqeurd" then
+        return true
+    end
+    if word == "sqioed" then
+        return true
+    end
+    if word == "squitd" then
+        return true
+    end
+    if word == "sqaud" then
+        return true
+    end
+    if word == "sqgad" then
+        return true
+    end
+    if word == "sqoxd" then
+        return true
+    end
+    if word == "sqod" then
+        return true
+    end
+    if word == "sq7d" then
+        return true
+    end
+    if word == "sq8d" then
+        return true
+    end
+    if word == "sqidz" then
+        return true
+    end
+    if word == "sqaid" then
+        return true
+    end
+    if word == "sqaiid" then
+        return true
+    end
+    if word == "sqciid" then
+        return true
+    end
+    if word == "sqyiid" then
+        return true
+    end
+    if word == "sqhiid" then
+        return true
+    end
+    if word == "sqiiud" then
+        return true
+    end
+    if word == "sqiud" then
+        return true
+    end
+    if word == "sq8id" then
+        return true
+    end
+    if word == "squi8d" then
+        return true
+    end
+    if word == "sq8uid" then
+        return true
+    end
+    if word == "squid8" then
+        return true
+    end
+    if word == "squ8id" then
+        return true
+    end
+    if word == "squid8" then
+        return true
+    end
+  
+    return false
+end
