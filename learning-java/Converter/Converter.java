e102fbc966e2377d837cb94fd579606474af6444
2garlicbread <155115950+2garlicbread@users.noreply.github.com>
2024-04-26 19:50:33 +1200

    Update Converter.java

--- learning-java/Converter/Converter.java
+++ learning-java/Converter/Converter.java
@@ -12,13 +12,19 @@ public class Converter {
 
         System.out.println("Converting...");
 
+        String[] newParams = removeFirstElement(params);
+
         switch (params[0]) {
             case "-f":
-                fahrenheitToCelcius(removeFirstElement(params));
+                fahrenheitToCelcius(newParams);
                 break;
 
             case "-h":
                 help();
+                break;
+
+            case "-c":
+                celciusToFahrenheit(newParams);
                 break;
 
             default:
@@ -27,17 +33,36 @@ public class Converter {
         }
     }
 
+    public static void celciusToFahrenheit(String[] numbers) {
+        float[] newNumbers = stringsToFloats(numbers);
+
+        for (float number : newNumbers) {
+            System.out.println((number + 32) * 9 / 5);
+        }
+    }
+
     public static void fahrenheitToCelcius(String[] numbers) {
-        for (String number : numbers) {
+        float[] newNumbers = stringsToFloats(numbers);
+
+        for (float number : newNumbers) {
+            System.out.println((number - 32) * 5 / 9);
+        }
+    }
+
+    public static float[] stringsToFloats(String[] strings) {
+        float[] floats = new float[strings.length];
+
+        for (int i = 0; i < strings.length; i++) {
+            String number = strings[i];
             number = number.replaceAll("[^\\d.]", "");
 
-            if (number.isEmpty()) number = "0";
-
+            if (number.isEmpty()) continue;
             float f = Float.parseFloat(number);
-            f = ((f - 32) * 5) / 9;
 
-            System.out.println(f);
+            floats[i] = f;
         }
+
+        return floats;
     }
 
     public static String[] removeFirstElement(String[] arr) {
@@ -53,6 +78,6 @@ public class Converter {
         System.out.print("""
             -h         |    Help.
             -f float[] |    Fahrenheit to Celcius.
-        """);
+        \n""");
     }
 }
