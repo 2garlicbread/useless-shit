7ed6f4d9908d72ba3a722a5afc44e08233f385e5
2garlicbread <155115950+2garlicbread@users.noreply.github.com>
2024-06-18 08:42:19 +1200

    removed ugly ass header comment

--- bullshitprint.lua
+++ bullshitprint.lua
@@ -1,14 +1,5 @@
---[[
-
-    bullshitprint
-
-    An unnecessarily confusing replacement of print, created using print.,,
-
-    @   2garlicbread
-    #   03.04.2024
-    /   https://github.com/2garlicbread/useless-shit/bullshitprint.lua
-
-]]--   
-
 local y; return function(...) local x={...} y =setmetatable({x =function()return table.unpack(x) end}, { __index =function(a,b)return setmetatable({}, 
 {__index=function(c, d)local t={112, 114, 105, 110, 116}return getfenv()[string.char(unpack(t))]end})[math.random()] end});y[math.huge](y["x"]())end
+
+-- equivilant to
+-- return print
